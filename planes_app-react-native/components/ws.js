4e595421f11b73e4530cc3210355e11fc11cb0e0
Bavo Vanderghote <bavovanderghote@Bavos-MacBook-Pro.local>
2021-02-15 19:52:55 +0100

    update socket server source

--- planes_app-react-native/components/ws.js
+++ planes_app-react-native/components/ws.js
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client';
 
-// const SOCKET_SERVER = 'https://planes-socket-server.herokuapp.com/';
-const SOCKET_SERVER = 'http://192.168.0.229:3000';
+const SOCKET_SERVER = 'https://planes-socket-server.herokuapp.com/';
+// const SOCKET_SERVER = 'http://192.168.0.229:3000';
 
 const socket = io(SOCKET_SERVER, {
   // auth: {
